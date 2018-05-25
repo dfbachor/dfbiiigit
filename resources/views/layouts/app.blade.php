41af6d269a6426aec7d8ec0ad64acd85d500b713
David Bachor <davidfbachor@gmail.com>
2018-05-25 16:42:25 -0400

    updated title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'dfbiii') }}</title>
+    <title>{{ app('system')->companyName }}</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
